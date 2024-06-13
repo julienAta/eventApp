0b49ab73310347745eda6b80a9d75cf2ef6bb454
Atanassov <jj.websolutions@proton.me>
2024-06-13 10:45:48 +0200

    next js init

---
+++ frontend/src/app/events/page.tsx
@@ -0,0 +1,20 @@
+// app/events/page.tsx
+import EventList from "../../components/EventList";
+import { FC } from "react";
+
+const fetchEvents = async () => {
+  const res = await fetch("http://localhost:3000/api/events");
+  if (!res.ok) {
+    throw new Error("Failed to fetch events");
+  }
+  return res.json();
+};
+
+const EventsPage: FC = async () => {
+  const events = await fetchEvents();
+  console.log(events, "events");
+
+  return <EventList events={events} />;
+};
+
+export default EventsPage;
