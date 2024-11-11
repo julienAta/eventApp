7ab08abc7c359071a5cb37f5993d1b7223e72b07
Atanassov <jj.websolutions@proton.me>
2024-11-11 16:49:58 +0100

    fix

--- client/src/lib/supabase-admin.ts
+++ client/src/lib/supabase-admin.ts
@@ -1,9 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseServiceKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6aW5vcGFrY2liemZyYXNqeXh6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxODI3NTQ0NSwiZXhwIjoyMDMzODUxNDQ1fQ.L4tuF9gs4F2i0BX5JtnkJN7CBTaluAWYo3ejy1RC-20";
+const supabaseServiceKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY;
 
+if (!supabaseServiceKey) {
+  throw new Error("Missing Supabase service key");
+}
 export const supabase = createClient(supabaseUrl, supabaseServiceKey, {
   auth: {
     autoRefreshToken: false,
